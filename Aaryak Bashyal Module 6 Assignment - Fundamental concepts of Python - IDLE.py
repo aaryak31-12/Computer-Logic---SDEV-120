a78cef6ce82a703820ecb6dc7088e6ad2ca41df2
aaryak31-12 <bashyalaaryak0@gmail.com>
2024-10-11 19:44:54 -0400

    Module 6 Assignment - Fundamental concepts of Python - IDLE

---
+++ Aaryak Bashyal Module 6 Assignment - Fundamental concepts of Python - IDLE.py
@@ -0,0 +1,7 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> c = "cats"
+>>> d = "dogs."
+>>> s = "It is raining " + c + " and " + d
+>>> print(s)
+It is raining cats and dogs.
