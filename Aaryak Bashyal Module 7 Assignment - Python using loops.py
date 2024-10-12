6b1d71a3b1abe055340b3f41f3f0ff24e16aa5bf
aaryak31-12 <bashyalaaryak0@gmail.com>
2024-10-11 21:39:33 -0400

    Module 7 Assignment - Python using loops

---
+++ Aaryak Bashyal Module 7 Assignment - Python using loops.py
@@ -0,0 +1,34 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+
+... 
+>>> 
+>>> 
+
+... 
+>>> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+>>> for number in numbers:
+...     if number % 2 == 0:
+...         print(str(number) + " is even ")
+...     else:
+...         print(str(number) + " is odd ")
+... 
+...         
+1 is odd 
+2 is even 
+3 is odd 
+4 is even 
+5 is odd 
+6 is even 
+7 is odd 
+8 is even 
+9 is odd 
+10 is even 
+11 is odd 
+12 is even 
+13 is odd 
+14 is even 
+15 is odd 
